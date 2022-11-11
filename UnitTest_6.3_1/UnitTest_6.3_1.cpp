4ff65fe005dedf1259cf90043a9023798466063f
viksi01 <victoria.pyatnicka@gmail.com>
2022-11-11 09:17:41 +0200

    master

---
+++ UnitTest_6.3_1/UnitTest_6.3_1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_6.3_1/lab_6.3_1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest631
+{
+	TEST_CLASS(UnitTest631)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int a[5] = { 92,71,14,-85,13 };
+			int t = Number(a, 5);
+			Assert::AreEqual(t, 4);
+
+		}
+	};
+}
---
+++ lab_6.3_1/lab_6.3_1.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+
+void Show(int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		a[i] = -100 + rand() % (-100 - 100) + 1;
+	}
+}
+
+void Print(int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << a[i] << ",";
+	}
+}
+
+int Number(int a[], int n)
+{
+	int k = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] > 0)
+		{
+			k++;
+		}
+	}
+	return k;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	const int n = 20;
+	int a[n];
+	Show(a, n);
+	cout << "a {"; Print(a, n); cout << "}" << endl;
+	cout << "Number of positive = " << Number(a, n) << endl;
+}
+
